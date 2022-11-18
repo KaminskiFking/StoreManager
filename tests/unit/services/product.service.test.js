b8d14f4cd70d0cf4013b200e2e216c63c68deca1
Vinicius Kaminski <vikaminski@hotmail.com>
2022-11-18 01:35:39 -0300

    Ajuste Test Product.Service

--- tests/unit/services/product.service.test.js
+++ tests/unit/services/product.service.test.js
@@ -5,6 +5,7 @@ const sinonChai = require('sinon-chai');
 const { expect } = chai;
 chai.use(sinonChai)
 
+const connection = require('../../../src/models/connection');
 const productsService = require('../../../src/services/products.service');
 const productModel = require('../../../src/models/product.model');
 
@@ -30,15 +31,21 @@ describe('Testes de unidade do Service de products', function () {
     expect(error.type).to.deep.equal(null);
     expect(error.message[0]).to.deep.equal(product);
   });
+  // it('Realizando uma operação DELETE BY ID com o Service Product', async function () {
+  //   sinon.stub(productModel, 'deleteByIdDataBase').resolves(1);
 
-  it('Realizando uma operação DELETE BY ID com o Service Product', async function () {
+  //   const error = await productsService.serviceDeleteProductbyId(1);
 
+  //   expect(error.type).to.deep.equal('PRODUCT_NOT_FOUND');
+  //   expect(error.message).to.deep.equal('Product not found');
+  // });
+  it('Realizando uma operação SUCESSUFUL DELETE BY ID com o Service Product', async function () {
     const product = { id: 1, name: 'test' }
-    sinon.stub(productModel, 'deleteByIdDataBase').resolves([product]);
+    sinon.stub(connection, 'execute').resolves([[product]]);
 
     const error = await productsService.serviceDeleteProductbyId(1);
 
-    expect(error.type).to.deep.equal('PRODUCT_NOT_FOUND');
-    expect(error.message).to.deep.equal('Product not found');
+    expect(error.type).to.deep.equal(null);
+    expect(error.message).to.deep.equal('Sucessuful Delete');
   });
 });
